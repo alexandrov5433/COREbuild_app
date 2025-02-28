d7f58b572b79fdb226bba926c4fdfb95c9a221a6
alexandrov5433 <alexandrov5433@gmail.com>
2025-02-28 22:36:58 +0100

    Popup message component redux. Styling and icon change based on message type.

--- src/components/general/popupMessage/PopupMessage.tsx
+++ src/components/general/popupMessage/PopupMessage.tsx
@@ -1,6 +1,6 @@
 import styles from './popupMessage.module.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faXmark, faCircleCheck, faCircleInfo, faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useState } from 'react';
 import { useAppSelector } from '../../../lib/hooks/reduxTypedHooks';
 
@@ -8,39 +8,40 @@ import { useAppSelector } from '../../../lib/hooks/reduxTypedHooks';
 export default function PopupMessage() {
     const messageData = useAppSelector(state => state.popupMessage);
     const [showMsg, setShowMsg] = useState(false);
+
     useEffect(() => {
         setShowMsg(messageData.isShown);
         const timeSetter = setTimeout(() => {
             setShowMsg(false);
         }, messageData.duration);
-        return () => clearTimeout(timeSetter);
+        return () => {
+            clearTimeout(timeSetter);
+        };
     }, [messageData.trigger]);
+
+    function iconPicker() {
+        switch (messageData.type) {
+            case 'success': return <FontAwesomeIcon icon={faCircleCheck} />;
+            case 'error': return <FontAwesomeIcon icon={faTriangleExclamation} />;
+            case 'neutral': return <FontAwesomeIcon icon={faCircleInfo} />;
+        }
+    }
+
     return (
-        <dialog open={showMsg} className={styles.wrapper}>
-
-            <div className={`${styles.content} ${styles[messageData.type]}`}>
-
+        <div className={`${styles.wrapper} ${styles[messageData.type]} ${showMsg ? styles.displayMsg : styles.hideMsg}`}>
+            <div className={`${styles.content}`}>
+                <div className={styles.iconContainer}>
+                    {iconPicker()}
+                </div>
                 <div className={styles.textContent}>
-                    <h5 className={styles.title}>
-                        {
-                            {
-                                success: 'Success!',
-                                error: 'Opps...!',
-                                neutral: ''
-                            }[messageData.type]
-                        }
-                    </h5>
                     <p className={styles.message}>{messageData.text}</p>
                 </div>
-
-                <div className={styles.buttonContainer}>
-                    <button onClick={() => setShowMsg(false)} className={styles.buttonElem}>
-                        <FontAwesomeIcon icon={faXmark} className={styles.buttonIcon}></FontAwesomeIcon>
-                    </button>
-                </div>
-
             </div>
-
-        </dialog>
+            <div className={styles.buttonContainer}>
+                <button onClick={() => setShowMsg(false)} className={styles.buttonElem}>
+                    <FontAwesomeIcon icon={faXmark} className={styles.buttonIcon}></FontAwesomeIcon>
+                </button>
+            </div>
+        </div>
     );
 }
