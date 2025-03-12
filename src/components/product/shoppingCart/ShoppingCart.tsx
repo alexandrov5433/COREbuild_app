18d6052c48c7d4cc740738c2feab5250511b0079
alexandrov5433 <alexandrov5433@gmail.com>
2025-03-12 21:12:09 +0100

    Product addition and removal added. Total sum for price added.

--- src/components/product/shoppingCart/ShoppingCart.tsx
+++ src/components/product/shoppingCart/ShoppingCart.tsx
@@ -1,34 +1,70 @@
 import styles from './shoppingCart.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEuroSign } from '@fortawesome/free-solid-svg-icons';
+import emptyCartImage from '../../../assets/emptyCart.svg';
 import { useAppSelector } from '../../../lib/hooks/reduxTypedHooks';
 import ShoppingCartProductCard from './shoppingCartProductCard/ShoppingCartProductCard';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { convertCentToWhole } from '../../../lib/util/currency';
+import { NavLink } from 'react-router';
 
 export default function ShoppingCart() {
+    const [listOfPrices, setListOfPices] = useState({} as {[key: number]: number});
     const [totalPrice, setTotalPrice] = useState(0);
     const cart = useAppSelector(state => state.cart);
 
-    function totalPriceSetterForCard(priceToAddInCent: number) {
-        setTotalPrice(state => state += priceToAddInCent);
+    function addPriceToListForCard(productID: number, priceInCent: number) {
+        setListOfPices(state => {
+            const newState = {...state};
+            const priceEntry = {};
+            (priceEntry as any)[productID] = priceInCent;
+            Object.assign(newState, priceEntry);
+            return newState;
+        });
     }
+
+    useEffect(() => {
+        if (cart && listOfPrices) {
+            let sum = 0;
+            Object.entries(listOfPrices).forEach(([key, val]) => {
+                if (Object.hasOwn(cart, key)) {
+                    sum += val
+                }
+            });
+            setTotalPrice(sum);
+        }
+    }, [listOfPrices]);
+
     return (
         <>
             <div className={styles.wrapper}>
                 <h1>Shopping Cart</h1>
                 {
-                    Object.entries(cart).map(([productID, count]) => <ShoppingCartProductCard
-                        key={productID}
-                        productID={Number(productID)}
-                        count={count}
-                        priceSetter={totalPriceSetterForCard}
+                    Object.entries(cart).length <= 0 ?
+                        <div className={styles.emptyCartWrapper}>
+                            <img src={emptyCartImage} className="img-fluid" alt="..." />
+                            <p className="lead">There are no items in your shopping cart.</p>
+                            <p className="lead">Please, add one or more of our products.</p>
+                            <NavLink className="btn btn-success" to="/products-catalog">Browse Products</NavLink>
+                        </div>
+                        :
+                        Object.entries(cart).map(([productID, count]) => <ShoppingCartProductCard
+                            key={productID}
+                            productID={Number(productID)}
+                            count={count}
+                            addToPriceList={addPriceToListForCard}
                         />)
                 }
             </div>
-            <nav className={`navbar sticky-bottom ${styles.totalCostBar}`}>
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">{convertCentToWhole(totalPrice)}</a>
-                </div>
-            </nav>
+            {
+                Object.entries(cart).length <= 0 ? '' :
+                    <nav className={`navbar sticky-bottom ${styles.totalCostBar}`}>
+                        <div className="container-fluid">
+                            <p className="lead">Total: {convertCentToWhole(totalPrice)} <FontAwesomeIcon icon={faEuroSign} /></p>
+                            <button className="btn btn-success">Place Order</button>
+                        </div>
+                    </nav>
+            }
         </>
     );
 }
--- src/components/product/shoppingCart/shoppingCartProductCard/ShoppingCartProductCard.tsx
+++ src/components/product/shoppingCart/shoppingCartProductCard/ShoppingCartProductCard.tsx
@@ -1,28 +1,123 @@
 import styles from './shoppingCartProductCard.module.css';
 import { useEffect, useState } from "react";
-import { ProductData } from "../../../../lib/definitions";
+import { ProductData, ShoppingCart } from "../../../../lib/definitions";
 import { convertCentToWhole } from "../../../../lib/util/currency";
 import productDetails from "../../../../lib/actions/productDetails";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faMinus, faEuroSign } from '@fortawesome/free-solid-svg-icons';
+import addProductToCart from '../../../../lib/actions/addProductToCart';
+import { useAppDispatch } from '../../../../lib/hooks/reduxTypedHooks';
+import { updateCart } from '../../../../redux/cartSlice';
+import { setMessageData } from '../../../../redux/popupMessageSlice';
+import removeProductFromCart from '../../../../lib/actions/removeProductFromCart';
 
 export default function ShoppingCartProductCard({
     productID,
     count,
-    priceSetter
+    addToPriceList
 }: {
     productID: number,
     count: number,
-    priceSetter: (priceToAddInCent: number) => void
+    addToPriceList: (productID: number, priceInCent: number) => void
 }) {
+    const dispatch = useAppDispatch();
     const [productData, setProductData] = useState({} as ProductData);
+    const [additionTrigger, setAdditionTrigger] = useState(false);
+    const [removalTrigger, setRemovalTrigger] = useState(false);
+    const [removeProductFromCartTrigger, setRemoveProductFromCartTrigger] = useState(false);
+
     useEffect(() => {
         (async () => {
             const actionResponse = await productDetails(productID);
             if (actionResponse.responseStatus === 200) {
                 setProductData(actionResponse.data!);
-                priceSetter(actionResponse.data?.price || 0);
+                addToPriceList(productID, actionResponse.data!.price * count || 0);
             }
         })();
-    }, []);
+    }, [productID]);
+
+    useEffect(() => {
+        if (productData.price) {
+            addToPriceList(productID, productData.price * count || 0);
+        }
+    }, [count, productData]);
+
+    useEffect(() => {
+        if (additionTrigger) {
+            (async () => {
+                const addition = await addProductToCart(productData.productID, 1);
+                if (addition.responseStatus === 200) {
+                    dispatch(updateCart(addition.data as ShoppingCart))
+                    dispatch(setMessageData({
+                        duration: 4000,
+                        isShown: true,
+                        text: 'Product added to cart!',
+                        type: 'success'
+                    }));
+                } else if ([400, 500].includes(addition.responseStatus)) {
+                    dispatch(setMessageData({
+                        duration: 5000,
+                        isShown: true,
+                        text: addition.msg,
+                        type: 'error'
+                    }));
+                }
+                setAdditionTrigger(false);
+            })();
+        }
+    }, [additionTrigger]);
+
+    useEffect(() => {
+        if (removalTrigger) {
+            (async () => {
+                const removal = await removeProductFromCart(productData.productID, 1);
+                if (removal.responseStatus === 200) {
+                    dispatch(updateCart(removal.data as ShoppingCart))
+                    dispatch(setMessageData({
+                        duration: 4000,
+                        isShown: true,
+                        text: 'Product removed from cart!',
+                        type: 'success'
+                    }));
+                } else if ([400, 500].includes(removal.responseStatus)) {
+                    dispatch(setMessageData({
+                        duration: 5000,
+                        isShown: true,
+                        text: removal.msg,
+                        type: 'error'
+                    }));
+                }
+                setRemovalTrigger(false);
+            })();
+        }
+    }, [removalTrigger]);
+
+    useEffect(() => {
+        if (removeProductFromCartTrigger) {
+            (async () => {
+                const removal = await removeProductFromCart(productData.productID, count);
+                if (removal.responseStatus === 200) {
+                    dispatch(updateCart(removal.data as ShoppingCart))
+                    dispatch(setMessageData({
+                        duration: 4000,
+                        isShown: true,
+                        text: 'Product removed from cart!',
+                        type: 'success'
+                    }));
+                    addToPriceList(productID, 0);
+                } else if ([400, 500].includes(removal.responseStatus)) {
+                    dispatch(setMessageData({
+                        duration: 5000,
+                        isShown: true,
+                        text: removal.msg,
+                        type: 'error'
+                    }));
+                }
+                setRemoveProductFromCartTrigger(false);
+            })();
+        }
+    }, [removeProductFromCartTrigger]);
+
     return (
         <div className="card mb-3">
             <div className={`row g-0 ${styles.cardWrapper}`}>
@@ -32,9 +127,26 @@ export default function ShoppingCartProductCard({
                 <div className="col-md-8">
                     <div className="card-body">
                         <h5 className="card-title">{productData.name}</h5>
-                        <p className={`card-text ${styles.price}`}>Price: {convertCentToWhole(productData.price)}</p>
-                        <p className={`card-text ${styles.count}`}>Count: {count}</p>
-                        <button className="btn btn-outline-warning">Remove Product</button>
+                        <p className={`card-text ${styles.price}`}>ID: {productData.productID}</p>
+                        <p className={`card-text ${styles.price}`}>Price: {convertCentToWhole(productData.price)} <FontAwesomeIcon icon={faEuroSign} /></p>
+                        <div className={styles.quantityWrapper}>
+                            <p className={`card-text ${styles.count}`}>Count: {count}</p>
+                            {
+                                count < productData.stockCount ?
+                                    <button className="btn btn-outline-success" disabled={additionTrigger} onClick={() => setAdditionTrigger(true)}>
+                                        <FontAwesomeIcon icon={faPlus} />
+                                    </button>
+                                    : ''
+                            }
+                            {
+                                count > 0 ?
+                                    <button className="btn btn-outline-danger" disabled={removalTrigger} onClick={() => setRemovalTrigger(true)}>
+                                        <FontAwesomeIcon icon={faMinus} />
+                                    </button>
+                                    : ''
+                            }
+                        </div>
+                        <button className="btn btn-outline-warning" disabled={removeProductFromCartTrigger} onClick={() => setRemoveProductFromCartTrigger(true)}>Remove Product</button>
                     </div>
                 </div>
             </div>
