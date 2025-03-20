d13233c9801ca14f5c49e7c8a3ae2e4c50502297
alexandrov5433 <alexandrov5433@gmail.com>
2025-03-20 23:14:54 +0100

    Home page ready.

--- src/components/general/home/Home.tsx
+++ src/components/general/home/Home.tsx
@@ -6,40 +6,63 @@ import banner2 from '../../../assets/banners/banner-2.png';
 import banner3 from '../../../assets/banners/banner-3.png';
 import banner4 from '../../../assets/banners/banner-4.png';
 import banner5 from '../../../assets/banners/banner-5.png';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareCheck, faPhone } from '@fortawesome/free-solid-svg-icons';
+
 import { NavLink } from 'react-router';
+import { useEffect, useState } from 'react';
+import { ProductData, ProductsCatalogQueryParams } from '../../../lib/definitions';
+import productsCatalog from '../../../lib/actions/productsCatalog';
+import ProductCard from '../../product/productsCatalog/productCard/ProductCard';
 
 export default function Home() {
+    const [prodcuts, setProducts] = useState([] as Array<ProductData>);
+    useEffect(() => {
+        (async () => {
+            const queryParams: ProductsCatalogQueryParams = {
+                currentPage: 1,
+                itemsPerPage: 8,
+                availableInStock: 'yes'
+            };
+            const actionResult = await productsCatalog(queryParams);
+            if (actionResult.responseStatus === 200) {
+                setProducts(actionResult.data!);
+            }
+        })();
+    }, []);
     return (
-        <div className={styles.wrapper}>
+        <div className={styles.homeWrapper}>
+            <h1>The <i>CORE</i> of your experience!</h1>
 
             <div id="carouselExampleAutoplaying" className={`carousel slide ${styles.carouselBody}`} data-bs-ride="carousel">
                 <div className="carousel-inner">
-                    <div className="carousel-item active">
+                    <div className="carousel-item active" data-bs-interval="3500">
                         <NavLink to={'/products-catalog'}>
                             <img src={banner} className="d-block w-100" alt="First banner image." />
                         </NavLink>
                     </div>
-                    <div className="carousel-item">
+                    <div className="carousel-item" data-bs-interval="3500">
                         <NavLink to={'/products-catalog'}>
                             <img src={banner1} className="d-block w-100" alt="Second banner image." />
                         </NavLink>
                     </div>
-                    <div className="carousel-item">
+                    <div className="carousel-item" data-bs-interval="3500">
                         <NavLink to={'/products-catalog'}>
                             <img src={banner2} className="d-block w-100" alt="Third banner image." />
                         </NavLink>
                     </div>
-                    <div className="carousel-item">
+                    <div className="carousel-item" data-bs-interval="3500">
                         <NavLink to={'/products-catalog'}>
                             <img src={banner3} className="d-block w-100" alt="Fourth banner image." />
                         </NavLink>
                     </div>
-                    <div className="carousel-item">
+                    <div className="carousel-item" data-bs-interval="3500">
                         <NavLink to={'/products-catalog'}>
                             <img src={banner4} className="d-block w-100" alt="Fifth banner image." />
                         </NavLink>
                     </div>
-                    <div className="carousel-item">
+                    <div className="carousel-item" data-bs-interval="3500">
                         <NavLink to={'/products-catalog'}>
                             <img src={banner5} className="d-block w-100" alt="Sixth banner image." />
                         </NavLink>
@@ -54,12 +77,73 @@ export default function Home() {
                     <span className="visually-hidden">Next</span>
                 </button>
             </div>
-            <iframe className='video'
-                title='Youtube player'
-                sandbox='allow-same-origin allow-forms allow-popups allow-scripts allow-presentation'
-                src={`https://youtube.com/embed/yQfhTh5hyxk?autoplay=0`}>
-            </iframe> 
-            {/* this portion yQfhTh5hyxk is the variable */}
+            <div className={styles.videosContainer}>
+                <section>
+                    <h4>Looking for a top-tier gaming configuration?</h4>
+                    <p className="lead">Building the ultimate gaming PC can be overwhelming with so many choices available. That's why we've got you covered! In this featured review, we take an in-depth look at one of the most powerful gaming rigs on the market.</p>
+
+                    <iframe className='video'
+                        title='Youtube player'
+                        sandbox='allow-same-origin allow-forms allow-popups allow-scripts allow-presentation'
+                        src={`https://youtube.com/embed/yQfhTh5hyxk?autoplay=0`}>
+                    </iframe>
+
+                </section>
+                <section>
+                    <h4>Finding the Perfect Gaming Monitor: What You Need to Know</h4>
+                    <p className="lead">Choosing the right gaming monitor is just as important as picking a powerful PC. A great display can enhance your gaming experience, providing smoother visuals, better colors, and faster response times. But with so many options—refresh rates, resolutions, panel types—how do you decide?</p>
+
+                    <iframe className='video'
+                        title='Youtube player'
+                        sandbox='allow-same-origin allow-forms allow-popups allow-scripts allow-presentation'
+                        src={`https://youtube.com/embed/mtA4hqZb4IE?autoplay=0`}>
+                    </iframe>
+                </section>
+                {/* this portion yQfhTh5hyxk is the variable */}
+            </div>
+            <div className={styles.servicesContainer}>
+                <div className={styles.contentContainer}>
+                    <h4>Our most wanted PC services</h4>
+                    <ul>
+                        <li>
+                            <p><FontAwesomeIcon icon={faSquareCheck} />Building & Shipping</p>
+                        </li>
+                        <li>
+                            <p><FontAwesomeIcon icon={faSquareCheck} />Cleaning & Repair</p>
+                        </li>
+                        <li>
+                            <p><FontAwesomeIcon icon={faSquareCheck} />Configuration</p>
+                        </li>
+                        <li>
+                            <p><FontAwesomeIcon icon={faSquareCheck} />Recycling Of Old Parts</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            {
+                !prodcuts.length ? '' :
+                    <>
+                        <h2>Featured products</h2>
+                        <div className={styles.featuredProdcutsContainerWrapper}>
+                            <div className={`row row-cols-1 row-cols-md-4 g-4 ${styles.featuredProdcutsContainer}`}>
+                                {
+                                    prodcuts.map(p => <ProductCard {...p} key={p.productID} />)
+                                }
+                            </div>
+                        </div>
+                    </>
+            }
+            <div className={styles.browseProdcutsButtonContainer}>
+                <NavLink className="btn btn-success" to={'/products-catalog'}>Browse All Prodcuts</NavLink>
+            </div>
+      
+            <div className={styles.contactUsContainer}>
+                <div className={styles.contentContainer}>
+                    <h4>Need assistance?</h4>
+                    <p>No problem! Our customer service is here to help.</p>
+                    <NavLink className="btn btn-outline-warning" to={'/contact-us'}>Contact Us</NavLink>
+                </div>
+            </div>
         </div>
     );
 }
