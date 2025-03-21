99512e30a76e22a9ee6ae3801044027ce62e18e1
alexandrov5433 <alexandrov5433@gmail.com>
2025-03-21 08:22:43 +0100

    Space removed.

--- src/components/product/productsCatalog/ProductsCatalog.tsx
+++ src/components/product/productsCatalog/ProductsCatalog.tsx
@@ -126,7 +126,6 @@ export default function ProductsCatalog() {
         })();
     }, [searchParams]);
 
-
     useEffect(() => {
         (async () => {
             const categoriesResult = await getAllProductCategories()
