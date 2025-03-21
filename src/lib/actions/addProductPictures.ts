dae8b048128c8dc54f1fbcbf6f67c86497f1cf01
alexandrov5433 <alexandrov5433@gmail.com>
2025-03-21 18:38:45 +0100

    Pictures addition action added.

---
+++ src/lib/actions/addProductPictures.ts
@@ -0,0 +1,32 @@
+import { ApiJsonResponce } from "../definitions";
+
+export default async function addProductPictures(productID: string, formData: FormData) {
+    const actionResponse = {
+        msg: '',
+        responseStatus: 0,
+        data: null,
+        isError: false
+    } as {
+        msg: string,
+        responseStatus: number,
+        data: null | boolean,
+        isError: boolean   
+    };
+    try {
+        const res = await fetch(`/api/add-product-pictures/${productID}`, {
+            method: 'post',
+            body: formData,
+            credentials: 'include'
+        });
+        const apiResponse = await res.json() as ApiJsonResponce;
+        actionResponse.responseStatus = res.status || 0;
+        actionResponse.msg = apiResponse.msg || '';
+        actionResponse.data = apiResponse.payload as boolean || null;
+    } catch (e) {
+        actionResponse.msg = (e as Error).message;
+        actionResponse.responseStatus = 0;
+        actionResponse.isError = true;
+    } finally {
+        return actionResponse;
+    }
+}
