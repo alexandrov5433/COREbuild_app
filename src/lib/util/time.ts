8b57aed8785cfeecc96f4028ce6b20d36c1fc10e
alexandrov5433 <alexandrov5433@gmail.com>
2025-04-07 20:41:12 +0200

    Time now shows the time also.

--- src/lib/util/time.ts
+++ src/lib/util/time.ts
@@ -1,5 +1,8 @@
 function convertTimeToDate(time: number) {
     const formater = new Intl.DateTimeFormat('en-GB', {
+        minute: '2-digit',
+        hour: '2-digit',
+        hourCycle: 'h24',
         day: '2-digit',
         month: 'short',
         year: 'numeric'
