df3be84ec43e9708ce428fcf1b3a3facef00ebed
alexandrov5433 <alexandrov5433@gmail.com>
2025-04-07 20:34:33 +0200

    Font resizing removed.

--- src/components/product/shoppingCart/ShoppingCart.tsx
+++ src/components/product/shoppingCart/ShoppingCart.tsx
@@ -77,7 +77,7 @@ export default function ShoppingCart() {
 
                     <nav className={`navbar sticky-bottom ${styles.totalCostBar}`}>
                         <div className={`container-fluid ${styles.innerContainer}`}>
-                            <p className="">Total: {convertCentToWholeString(totalPrice)} <FontAwesomeIcon icon={faEuroSign} /></p>
+                            <p className="lead">Total: {convertCentToWholeString(totalPrice)} <FontAwesomeIcon icon={faEuroSign}/></p>
                             <p className="lead">With tax and free shipping.</p>
                             <button className={`btn btn-success ${styles.buttonGreenGlow}`} onClick={() => {
                                 (paypayCheckoutRef.current! as HTMLDivElement).scrollIntoView({
